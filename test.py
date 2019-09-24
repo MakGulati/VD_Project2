4d224f5366d838268d26c48b705d8ab76d78baad
MAYANK GULATI <mayank_gulati@stu.upes.ac.in>
2019-09-24 18:14:44 +0200

    merged1

--- test.py
+++ test.py
@@ -6,12 +6,16 @@ import cv2
 import matplotlib.pyplot as plt
 
 # (a)
+# class matrix():
+#     def __init__(self):
+#     def __len__(self):
+#     def
 
 # dir_path_database = "D:/Federico/Documents/Federico/Uni Trento/03 Magistrale EIT/02 EIT VCC 2019-20/1st period/Analysis and Search of Visual Data EQ2425/Projects/Project 2/Data2/server/obj"
 dir_path_database = 'Data2/server/obj'
+counter = 0
 
-for i in range(1): #50 objects, 3 images per time
-
+for i in range(50): #50 objects, 3 images per time
     img1 = cv2.imread(dir_path_database + str(i+1) + "_1.jpg", cv2.IMREAD_GRAYSCALE)
     sift = cv2.xfeatures2d.SIFT_create(contrastThreshold = 0.2, edgeThreshold = 9.3)
     kp1, des1 = sift.detectAndCompute(img1, None)
@@ -34,9 +38,13 @@ for i in range(1): #50 objects, 3 images per time
                 good.append(m.queryIdx)
         m2 = np.delete(j, good,0)
         des=np.vstack((des,m2))
-
+    counter+=des.shape[0]
 
 print(des.shape)
+print(counter/50)
+
+
+
 
 
 
