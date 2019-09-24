0141cdeadfa4822454b45fba7a7409dd3c47fe36
MAYANK GULATI <mayank_gulati@stu.upes.ac.in>
2019-09-24 22:57:16 +0200

    part a done

--- test.py
+++ test.py
@@ -6,17 +6,25 @@ import cv2
 import matplotlib.pyplot as plt
 
 # (a)
-# class matrix():
-#     def __init__(self):
-#     def __len__(self):
-#     def
+class object():
+    def __init__(self,_obj_id,_des_id):
+        self.obj_id=_obj_id
+        self.des_id=_des_id
+    def __len__(self):
+        return(self.des_id.shape[0])
+
+    def get_des(self,_pos_id):
+        return (self.des_id[_pos_id])
+
+    def __del__(self):
+        pass
+
 
 #dir_path_database = "D:/Federico/Documents/Federico/Uni Trento/03 Magistrale EIT/02 EIT VCC 2019-20/1st period/Analysis and Search of Visual Data EQ2425/Projects/Project 2/Data2/server/obj"
 dir_path_database = 'Data2/server/obj'
 counter = 0
-
-for i in range(1): #50 objects, 3 images per time
-    print('i =',i+1)
+abc={}
+for i in range(50): #50 objects, 3 images per time
     img1 = cv2.imread(dir_path_database + str(i+1) + "_1.jpg", cv2.IMREAD_GRAYSCALE)
     sift = cv2.xfeatures2d.SIFT_create(250) #strongest 250
     kp1, des1 = sift.detectAndCompute(img1, None)
@@ -36,27 +44,16 @@ for i in range(1): #50 objects, 3 images per time
                 good.append(m.queryIdx) #saving index for removing it
         m2 = np.delete(j, good, 0) #removing matching features in same object
         des = np.vstack((des, m2))
-#            print("j=",j.shape[0])
-#            print("m2=",m2.shape[0])
-#            des = np.vstack((des, m2))
-#            print("des",des.shape[0])
-            
-#        if i == 31:
-#            print(len(good))
-#            m2 = np.delete(j, good, 0) #removing matching features in same object
-#            print("j=",j.shape[0])
-#            print("m2=",m2.shape[0])
-#            des = np.vstack((des, m2))
-#            print("des",des.shape[0])
 
-    index = [np.repeat(i+1,des.shape[0])]
-    desmak=np.hstack((des,index))
-    print('#desc per obj =', desmak.shape) #number of descriptors for an object
-    counter += des.shape[0]
-    print(' ')
+    abc[i]=object(i+1, des)
+    counter+=abc[i].__len__()
+
+
+
+
     
     
-
+print(abc[1].des_id)
 #avg n°feature extracted per database object
 avg_feature_database_object = counter / 50
 print('Avg # feature per object = ', avg_feature_database_object)
