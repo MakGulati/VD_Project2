085a12737ce0789c8797ba6fa5829a0b022c63cf
MAYANK GULATI <mayank_gulati@stu.upes.ac.in>
2019-09-24 21:32:05 +0200

    mayank up

--- test.py
+++ test.py
@@ -11,16 +11,16 @@ import matplotlib.pyplot as plt
 #     def __len__(self):
 #     def
 
-dir_path_database = "D:/Federico/Documents/Federico/Uni Trento/03 Magistrale EIT/02 EIT VCC 2019-20/1st period/Analysis and Search of Visual Data EQ2425/Projects/Project 2/Data2/server/obj"
-#dir_path_database = 'Data2/server/obj'
+#dir_path_database = "D:/Federico/Documents/Federico/Uni Trento/03 Magistrale EIT/02 EIT VCC 2019-20/1st period/Analysis and Search of Visual Data EQ2425/Projects/Project 2/Data2/server/obj"
+dir_path_database = 'Data2/server/obj'
 counter = 0
 
-for i in range(50): #50 objects, 3 images per time
+for i in range(1): #50 objects, 3 images per time
     print('i =',i+1)
     img1 = cv2.imread(dir_path_database + str(i+1) + "_1.jpg", cv2.IMREAD_GRAYSCALE)
-    sift = cv2.xfeatures2d.SIFT_create(300) #strongest 300 hundred
+    sift = cv2.xfeatures2d.SIFT_create(250) #strongest 250
     kp1, des1 = sift.detectAndCompute(img1, None)
-    des = np.vstack(des, des1)
+    des = des1
     img2 = cv2.imread(dir_path_database + str(i+1) + "_2.jpg", cv2.IMREAD_GRAYSCALE)
     kp2, des2 = sift.detectAndCompute(img2, None)
     img3 = cv2.imread(dir_path_database + str(i+1) + "_3.jpg", cv2.IMREAD_GRAYSCALE)
@@ -34,15 +34,28 @@ for i in range(50): #50 objects, 3 images per time
         for m, n in matches:
             if m.distance < 0.75 * n.distance: #if it is a match
                 good.append(m.queryIdx) #saving index for removing it
-        if i == 31:
-            print('len good =', len(good))
         m2 = np.delete(j, good, 0) #removing matching features in same object
         des = np.vstack((des, m2))
+#            print("j=",j.shape[0])
+#            print("m2=",m2.shape[0])
+#            des = np.vstack((des, m2))
+#            print("des",des.shape[0])
+            
+#        if i == 31:
+#            print(len(good))
+#            m2 = np.delete(j, good, 0) #removing matching features in same object
+#            print("j=",j.shape[0])
+#            print("m2=",m2.shape[0])
+#            des = np.vstack((des, m2))
+#            print("des",des.shape[0])
 
-    print('#desc per obj =', des.shape[0]) #number of descriptors for an object
+    index = [np.repeat(i+1,des.shape[0])]
+    desmak=np.hstack((des,index))
+    print('#desc per obj =', desmak.shape) #number of descriptors for an object
     counter += des.shape[0]
     print(' ')
-    des = [] #clear des
+    
+    
 
 #avg n°feature extracted per database object
 avg_feature_database_object = counter / 50
