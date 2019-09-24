c0eb1ff229a757b9f80276182fd9459e7fe6429e
federico_favia <federic.fav@gmail.com>
2019-09-24 09:45:05 +0200

    New files

--- test.py
+++ test.py
@@ -21,20 +21,28 @@ des = []
 data_path = os.path.join(img_dir,'*g')
 files = glob.glob(data_path)
 #data = []
-m = np.empty([160, 128])
+#m = np.empty()
 
-for f1 in files:
-    img = cv.imread(f1, cv.IMREAD_GRAYSCALE)
-    #data.append(img)
-    #cv.imshow('im', img)
-    #cv.waitKey(50)
-    sift = cv.xfeatures2d.SIFT_create(160) #strongest 150
-    kp1, des1 = sift.detectAndCompute(img, None)
-    for i in range(160):
-        m[i] = des1[i]
-        #print(des1[0])
+#for f1 in files:
+dir = "D:/Federico/Documents/Federico/Uni Trento/03 Magistrale EIT/02 EIT VCC 2019-20/1st period/Analysis and Search of Visual Data EQ2425/Projects/Project 2/Data2/server/obj"
 
-print("printing lists in new line")
-print(m.shape)
+for i in range(2):
+    final = np.empty((2*750, 128))
 
-#print(*des, sep="\n")
+    for j in range(3):
+        img = cv.imread(dir+str(i+1)+"_"+str(j+1)+".jpg", cv.IMREAD_GRAYSCALE)
+        #data.append(img)
+        #cv.imshow('im', img)
+        #cv.waitKey(0)
+        sift = cv.xfeatures2d.SIFT_create(249) #strongest 150
+        sift = cv.xfeatures2d.SIFT_create(contrastThreshold=)
+        kp1, des1 = sift.detectAndCompute(img, None)
+        np.concatenate((final, des1), axis = 0)
+
+new = final.reshape(-1,750,128)
+
+#print("printing lists in new line")
+print(final.shape)
+print(new.shape)
+
+
