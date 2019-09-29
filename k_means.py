e122f0681d28889cf56b13f82cf019bbf2a781ab
MAYANK GULATI <mayank_gulati@stu.upes.ac.in>
2019-09-29 23:57:08 +0200

    working clusters

--- k_means.py
+++ k_means.py
@@ -7,16 +7,38 @@ def hi_kmeans(_des_database_list, _b): #need to add depth
     for i in range(1,50):
         X = np.vstack((X,_object[i].des_mat))
     '''
-
+    print('tot features', len(_des_database_list))
+    
     X=[]
     for i in range(len(_des_database_list)):
         X.append(_des_database_list[i].vector)
+    
+    X_new=np.array(X)
 
-    clusters = KMeans(n_clusters=_b, random_state=0).fit(X)
-    y_kmeans = clusters.predict(X)
+        
+    kmeans = KMeans(n_clusters=_b, random_state=0).fit(X_new)
+    #y_kmeans = clusters.predict(X_new)
+    #print(y_kmeans)
+    kmeans_labels = kmeans.labels_
+    #print(kmeans_labels)
     #print(X.shape[0])
-    plt.scatter(X[:, 0], X[:, 127], c=y_kmeans, s=50, cmap='viridis')
 
-    centers = clusters.cluster_centers_
+    clusters = [[] for i in range (_b)]
+
+    for m in range(_b):
+        for x, y in zip(X_new, kmeans_labels):
+            if y==m:
+                clusters[m].append(x)
+        print ("m",len(clusters[m]))
+
+
+
+
+
+
+
+    #plt.scatter(X_new[:, 0], X_new[:, 127], c=kmeans_labels, s=50, cmap='viridis')
+
+    centers = kmeans.cluster_centers_
     # plt.scatter(centers[:, 0], centers[:, 127],c='red',alpha=0.9)
     # plt.show()
--- test.py
+++ test.py
@@ -10,12 +10,12 @@ from treelib import *
 ## 2 IMAGE FEATURE EXTRACTION
 # (a) Extract few hundreds features from each database image and combine the ones for same object, avg n°features per database object
 
-n_keypoints = 250  # strongest keypoints to keep
+n_keypoints = 200  # strongest keypoints to keep
 b = 3 #n of brances (cluster) in each level of tree
 
 # directory path with database images
-dir_path_database = "D:/Federico/Documents/Federico/Uni Trento/03 Magistrale EIT/02 EIT VCC 2019-20/1st period/Analysis and Search of Visual Data EQ2425/Projects/Project 2/Data2/server/obj"
-#dir_path_database = 'Data2/server/obj'
+# dir_path_database = "D:/Federico/Documents/Federico/Uni Trento/03 Magistrale EIT/02 EIT VCC 2019-20/1st period/Analysis and Search of Visual Data EQ2425/Projects/Project 2/Data2/server/obj"
+dir_path_database = 'Data2/server/obj'
 
 tot_features_database = 0  # counting total features of database for retrieving average
 database_des = {}  # dictionary of database objects containing descriptors
@@ -36,7 +36,7 @@ for i in range(50):  # 250 images with 50 objects, 3 images per object read at t
         matches = bf.knnMatch(des1, j, k=2)
         good = []
         for m, n in matches:
-            if m.distance < 0.75 * n.distance:  # if it is a match
+            if m.distance < 0.80 * n.distance:  # if it is a match
                 good.append(m.queryIdx)  # saving index for removing it
         m2 = np.delete(j, good, 0)  # removing matching features in same object
         des = np.vstack((des, m2))
