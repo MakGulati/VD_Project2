e2d4810bbc76c348783237f3e3f74d981e4ae6d1
federico_favia <federic.fav@gmail.com>
2019-09-30 18:25:41 +0200

    update kmeans

--- k_means.py
+++ k_means.py
@@ -7,19 +7,18 @@ from treelib import *
 def hi_kmeans(_first_node, _des_database_list, _b, _depth):  # need to add depth
 
     # print('tot features', len(_des_database_list))
-    X = []
+    descriptors = []
     for i in range(len(_des_database_list)):
-        X.append(_des_database_list[i].vector)
+        descriptors.append(_des_database_list[i].vector)
 
-    X_new = np.array(X)
+    descriptors_new = np.array(descriptors) # transform list to np array for kmeans
 
-    kmeans = KMeans(n_clusters=_b, random_state=0).fit(X_new)
-    kmeans_labels = kmeans.labels_
-    #print(len(kmeans_labels))
+    kmeans = KMeans(n_clusters=_b, random_state=0).fit(descriptors_new) # kmeans clustering algorithm
+    kmeans_labels = kmeans.labels_ #obtaining the labels of the clusters
 
     clusters = [[] for i in range(_b)]
     kmeans_centroids = kmeans.cluster_centers_
-    centroids = kmeans_centroids.tolist()
+    centroids = kmeans_centroids.tolist() # for storing centroids in each node
 
     '''
     for m in range(_b):
@@ -31,6 +30,7 @@ def hi_kmeans(_first_node, _des_database_list, _b, _depth):  # need to add depth
     centroids  = kmeans.cluster_centers_
     '''
 
+    # code to check
     for a in range(len(kmeans_labels)):  # total numbers of descriptors
         tmp_list = []
         if len(clusters[kmeans_labels[a]]) > 0:
@@ -42,7 +42,7 @@ def hi_kmeans(_first_node, _des_database_list, _b, _depth):  # need to add depth
     if _depth > 0:
         _depth -= 1
         for m in range(_b):
-            _child = Tree(clusters[m], centroids[m])
-            _first_node.addChild(_child)
-            hi_kmeans(_child, clusters[m], _b, _depth)
+            _child = Tree(clusters[m], centroids[m]) # child
+            _first_node.addChild(_child) # adding the child to the parent
+            hi_kmeans(_child, clusters[m], _b, _depth) # kmeans clustering on each child
         # _first_node.nestedTree()
