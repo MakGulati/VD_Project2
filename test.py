9b1dbc76b4bf4915b8c138f38d9a583a779d1987
federico_favia <federic.fav@gmail.com>
2019-10-02 16:45:29 +0200

    Last details
    We still need to modify file names

--- test.py
+++ test.py
@@ -13,14 +13,14 @@ warnings.filterwarnings("ignore", category=DeprecationWarning) #remove Deprecati
 ## 2 IMAGE FEATURE EXTRACTION
 # (a) Extract few hundreds features from each database image and combine the ones for same object, avg n°features per database object
 
-n_documents = 50 # n of documents (buildings) presents in database
-n_queries = 50 # n of query images
+n_documents = 50  # n of documents (buildings) presents in database
+n_queries = 50  # n of query images
 n_keypoints = 350  # strongest keypoints to keep 350
-nndr_thresh = 0.80 # thresh for nndr SIFT match
+nndr_thresh = 0.80  # thresh for nndr SIFT match
 
 # directory path with database images
-# dir_path_database = "D:/Federico/Documents/Federico/Uni Trento/03 Magistrale EIT/02 EIT VCC 2019-20/1st period/Analysis and Search of Visual Data EQ2425/Projects/Project 2/Data2/server/obj"
-dir_path_database = 'Data2/server/obj'
+dir_path_database = "D:/Federico/Documents/Federico/Uni Trento/03 Magistrale EIT/02 EIT VCC 2019-20/1st period/Analysis and Search of Visual Data EQ2425/Projects/Project 2/Data2/server/obj"
+# dir_path_database = 'Data2/server/obj'
 
 # merging features for database images
 tot_features_database = 0  # counting total features of database for retrieving average
@@ -36,7 +36,7 @@ for i in range(n_documents):  # 250 images with 50 buildings (documents), 3 imag
     img3 = cv2.imread(dir_path_database + str(i + 1) + "_3.jpg", cv2.IMREAD_GRAYSCALE)
     kp3, des3 = sift.detectAndCompute(img3, None)
 
-    for j in [des2, des3]: # comparing same building images for removing redundant SIFT descritpors
+    for j in [des2, des3]:  # comparing same building images for removing redundant SIFT descritpors
         bf = cv2.BFMatcher()
         matches = bf.knnMatch(des1, j, k=2)
         good = []
@@ -49,7 +49,7 @@ for i in range(n_documents):  # 250 images with 50 buildings (documents), 3 imag
     des_database[i] = keypoints_mat_with_id(des, i)
     tot_features_database += des_database[i].__len__()
 
-# avg n°feature extracted per database object
+# Avg n° feature extracted per database object
 avg_feature_database_object = tot_features_database / len(des_database)
 print('Avg # feature per database object = ', avg_feature_database_object)
 
@@ -57,21 +57,23 @@ print('Avg # feature per database object = ', avg_feature_database_object)
 # (b) Extract few hundreds features from each query image and save them separately, avg n°features per query object
 
 # directory path with query images
-# dir_path_query = "D:/Federico/Documents/Federico/Uni Trento/03 Magistrale EIT/02 EIT VCC 2019-20/1st period/Analysis and Search of Visual Data EQ2425/Projects/Project 2/Data2/client/obj"
-dir_path_query ='Data2/client/obj'
+dir_path_query = "D:/Federico/Documents/Federico/Uni Trento/03 Magistrale EIT/02 EIT VCC 2019-20/1st period/Analysis and Search of Visual Data EQ2425/Projects/Project 2/Data2/client/obj"
+# dir_path_query ='Data2/client/obj'
 
-tot_features_query = 0 # counting total features of database for retrieving average
-des_query = {} # dictionary of query objects containing descriptors
+tot_features_query = 0  # counting total features of database for retrieving average
+des_query = {}  # dictionary of query objects containing descriptors
+
+factors = [1, 0.9, 0.7, 0.5]
 
 for i in range(n_queries): # 50 query images
     img_i = cv2.imread(dir_path_query+str(i+1)+"_t1.jpg", cv2.IMREAD_GRAYSCALE)
-    sift_i = cv2.xfeatures2d.SIFT_create(n_keypoints)
+    sift_i = cv2.xfeatures2d.SIFT_create(factors[0] * n_keypoints)
     kp_i, des_i = sift_i.detectAndCompute(img_i, None)
 
     des_query[i] = keypoints_mat_with_id(des_i, i)
     tot_features_query += des_query[i].__len__()
 
-# avg n°feature extracted per query object
+# Avg n° feature extracted per query object
 avg_feature_query_object = tot_features_query / len(des_query)
 print("Avg # feature per query object = ", avg_feature_query_object)
 
@@ -94,7 +96,6 @@ hi_kmeans(parent_node, des_database_list, b, depth, n_documents)  # b is number 
 
 print("Tree has been built! Now querying...")
 
-top1_first_tree = []
 counter_t1 = 0
 counter_t5 = 0
 
@@ -132,9 +133,8 @@ for i in range(n_queries):
     top5_items = sorted(accu_list,reverse=True)[:5]
     for p in range(5):
         top5.append(accu_list.index(top5_items[p]))
-    print('image ',i,'top5 ',top5)
-    print('image ',i,' classified as image ', top1)
-
+    print('image ', i,'top5 ',top5)
+    print('image ', i,' classified as image ', top1)
 
     if top1 == i: # image correctly classified?
         counter_t1 += 1
@@ -143,25 +143,15 @@ for i in range(n_queries):
     if i in top5:
         counter_t5 += 1
 
-# avg top-1 recall rate
-avg_recall_rate1 = counter_t1 / n_queries
-print("Avg recall rate_t1 = ", avg_recall_rate1)
+# Avg top-1 recall rate
+avg_recall_rate_t1 = counter_t1 / n_queries
+print("Avg recall rate top-1 = ", avg_recall_rate_t1)
 
-avg_recall_rate2 = counter_t5 / n_queries
-print("Avg recall rate_t5 = ", avg_recall_rate2)
+# Avg top-5 recall rate
+avg_recall_rate_t5 = counter_t5 / n_queries
+print("Avg recall rate top-5 = ", avg_recall_rate_t5)
 
-# first_tree_child_data = first_tree[0].data
-# second_tree_child_centroid = first_tree[1].centroid
 
-# building 2nd tree (b=5, depth=4)
-# b = 5 # n of branches (clusters) in each level of tree
-# depth = 4 # n of levels of tree
-# hi_kmeans(parent_node, des_database_list, b, depth, n_documents)  # b is number of clusters, depth is number of levels
-
-# building 3rd tree (b=5, depth=7)
-# b = 5 # n of branches (clusters) in each level of tree
-# depth = 7 # n of levels of tree
-# hi_kmeans(parent_node, des_database_list, b, depth, n_documents)  # b is number of clusters, depth is number of levels
 
 
 
